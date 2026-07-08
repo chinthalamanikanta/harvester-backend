38f64ddaf8959a11d32b1d54454c728fb8073179
chinthalamanikanta <chm80076@gmail.com>
2026-07-08 09:46:01 +0530

    Add db part

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = (
     "mysql+pymysql://maniadmin:admin%40123@"
     "mani-server-db.mysql.database.azure.com:3306/harvester_db"
+    "?ssl_mode=REQUIRED"
 )
 
 # DATABASE_URL = (
